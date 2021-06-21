1ae3e69f4773fe9804aed97ae1122875e5e4a4a6
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 00:05:33 +0100

    Create 10-8 (extra).py

---
+++ 10-8 (extra).py
@@ -0,0 +1,17 @@
+a = 1
+factorial = 1
+while a <=4:
+    factorial = factorial * a
+    a = a +1
+print(factorial)
+
+#if you wanted you could use while on the factorial instead
+
+a = 1
+factorial = 1
+while factorial <24:
+    factorial = factorial * a
+    a = a +1
+print(factorial)
+
+#additionally < 5 or <= 23 works too 
