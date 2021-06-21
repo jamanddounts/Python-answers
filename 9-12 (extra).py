73e6e53b3f96c472c7964196e902bf680ccad54f
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-21 07:21:29 +0100

    Create 9-12 (extra).py

---
+++ 9-12 (extra).py
@@ -0,0 +1,14 @@
+for x in range(1,11):
+    even = x%2
+    if even==0:
+        print(x)
+    else:
+        ()
+
+#the sexy gyals are doing
+
+for x in range(1,11):
+    if x%2==0:
+        print(x)
+    else:
+        () 
