86cf9e3189cf01a6c16b49d64d9b7c2d51420a21
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-21 07:51:10 +0100

    Create 10-6.py

---
+++ 10-6.py
@@ -0,0 +1,9 @@
+a = int(input("Input integer:"))
+b = 1
+while b<=a:
+    print(b)
+    b = b + 1
+
+#there is deffo another way to do this...
+#i cant think of it rn but i would assume you'd reverse a and b when checking
+#if anyone finds another way, plz hmu
