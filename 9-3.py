e6766d3210e3f910a5a7ee4069b536c108910931
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-21 06:48:49 +0100

    Create 9-3.py

---
+++ 9-3.py
@@ -0,0 +1,9 @@
+subjects = ["Japanese", "Mathematics", "Science", "Sociology", "English"]
+for subject in subjects:
+    print(subject)
+    
+#i dont think this matters but im gonna complain for a sec
+#in their tutorial they want you to use the singular version of the list name (ie subject)
+#in reality you can use whatever you want (people normally use x iirc)
+#so ye fuck them for this
+#that said the above is like "The Perfect Answer" but you can just use whatever variable names you'd like
