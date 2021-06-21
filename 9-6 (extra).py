d3ff060ab01c12e8ed3744ca9198bf1a0670234e
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-21 07:02:48 +0100

    Create 9-6 (extra).py

---
+++ 9-6 (extra).py
@@ -0,0 +1,9 @@
+scores = [37, 39, 84, 3, 43, 42, 87, 78, 19, 22]
+total = 0
+for x in scores:
+    total = total + x
+average = total/len(scores)
+print(average)
+
+#if you want to be sexy you can combine 5 and 6
+print(total/len(scores))
