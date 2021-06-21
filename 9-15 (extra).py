d1962ad9bc6643e6eab9355dee377cc68be48d1d
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-21 07:29:16 +0100

    Create 9-15 (extra).py

---
+++ 9-15 (extra).py
@@ -0,0 +1,11 @@
+a = input("Input string:")
+a_count = 0
+for x in a:
+    if x=="a":
+        a_count = a_count + 1
+print(a_count)
+
+#you could have
+    else:
+        ()
+#after the if statement if you felt like it      
