35b97ca438ddde4d9b1131783d83a68f77858d6d
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-21 07:46:13 +0100

    Create 10-5.py

---
+++ 10-5.py
@@ -0,0 +1,8 @@
+i = 10
+while i >= 1:
+    print(i)
+    i = i-1
+    
+#that said
+while i > 0:
+#works too but... since the instructions say "greater than or equal to" i guess it's not the preferred answer
