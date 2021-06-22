6146d7effebf9dedbebc891f25b75f912e08bdb6
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 02:51:14 +0100

    Create 12-17 (extra).py

---
+++ 12-17 (extra).py
@@ -0,0 +1,14 @@
+scores = {}
+for n in range(5):
+    new_name = input("Input name:")
+    new_score = int(input("Input score:"))
+    scores[new_name] = new_score 
+print("Passed students are as follows")
+for k, v in scores.items():
+    if v>=70 and v<=100:
+        print(k)
+        
+#these bonus assignments are way too hard what the fuck?
+#there are probably alternative solutions but just finding this took way too long, i dont want to think about it
+#if anyone found a different solution plz hmu
+#the two pages about keys() and values() before this exercise were absolutely useless why did they add them
