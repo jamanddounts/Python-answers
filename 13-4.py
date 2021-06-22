b5fae980e721fb54c7adc3e1ee353430a558ff78
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 03:11:48 +0100

    Create 13-4.py

---
+++ 13-4.py
@@ -0,0 +1,9 @@
+def hello_from_function():
+    print('Hello from functions!')
+
+print('Hello from outside the function!')
+hello_from_function()
+print('Hello from outside the function, once again!')
+
+# Call the hello_from_function() once more 
+hello_from_function()
