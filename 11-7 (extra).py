65170ccc5e1cefef02f96931e58c553a7c7942fd
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 01:13:06 +0100

    Create 11-7 (extra).py

---
+++ 11-7 (extra).py
@@ -0,0 +1,11 @@
+import random
+
+rps = ["rock", "paper", "scissors"]
+rpscom = random.choice(rps)
+print("rock-paper-scissors", rpscom)
+
+#or you could do this instead
+import random
+
+rps = ["rock", "paper", "scissors"]
+print("rock-paper-scissors", random.choice(rps))
