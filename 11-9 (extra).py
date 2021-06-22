63c77ce9ac5333cd1d370756c82642dc474042d8
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 01:20:13 +0100

    Create 11-9 (extra).py

---
+++ 11-9 (extra).py
@@ -0,0 +1,13 @@
+import math
+a = int(input("Input integer:"))
+b = int(input("Input integer:"))
+gcd = math.gcd(a,b)
+print("Greatest common divisor", gcd)
+
+#or if you want to shorten it
+import math
+a = int(input("Input integer:"))
+b = int(input("Input integer:"))
+print("Greatest common divisor", math.gcd(a,b))
+
+#who the fuck calls it gcd? hcf homies where you at
