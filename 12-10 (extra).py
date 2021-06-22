657a23deba83e0d44dd0ccb36eb6b5cf6bea4c6f
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 01:46:54 +0100

    Create 12-10 (extra).py

---
+++ 12-10 (extra).py
@@ -0,0 +1,8 @@
+cities = {'Tokyo': 13.929, 'New York City': 8.398, 'London': 8.908}
+print(cities['New York City'])
+cities['Paris'] = 10.901
+print(cities)
+print(len(cities))
+del cities['London']
+print(cities)
+print(len(cities))
