3898b065023c43d92220aa1ce621d37d7e164b46
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 03:33:38 +0100

    Create 13-13.py

---
+++ 13-13.py
@@ -0,0 +1,7 @@
+def print_meow(cnt):
+    for i in range(cnt):
+        print('meow!')
+
+# Enter an integer value using the keyboard and pass it to the function print_meow
+n = int(input("Count:"))
+print_meow(n)
