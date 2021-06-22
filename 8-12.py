8617b9129d597ebf6432730b535573bf940c4d41
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 06:00:08 +0100

    Update and rename 8-12 to 8-12.py

---
+++ 8-12.py
@@ -0,0 +1,8 @@
+numbers = [1,3,5,7]
+look_for = int(input("Input integer:"))
+if look_for in numbers:
+    print("in the list")
+else:
+    print("not in the list")
+    
+#look4_for can be renamed to whatever you want
