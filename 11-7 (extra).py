883450a3d689a02d6b525c87c8f4cac4de38f682
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 01:13:29 +0100

    Update 11-7 (extra).py

--- 11-7 (extra).py
+++ 11-7 (extra).py
@@ -1,11 +1,9 @@
 import random
-
 rps = ["rock", "paper", "scissors"]
 rpscom = random.choice(rps)
 print("rock-paper-scissors", rpscom)
 
 #or you could do this instead
 import random
-
 rps = ["rock", "paper", "scissors"]
 print("rock-paper-scissors", random.choice(rps))
