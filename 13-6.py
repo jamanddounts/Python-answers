974b7e6879c7368bbcc92bfb30aabdaf21d0ab67
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 03:16:33 +0100

    Create 13-6.py

---
+++ 13-6.py
@@ -0,0 +1,10 @@
+# Define the print_hi function here to print “Hi” 
+def print_hi():
+    print("Hi")
+# Define the print_bye function here to print “Bye”
+def print_bye():
+    print("Bye")
+    
+print_hi()
+print_hi()
+print_bye()
