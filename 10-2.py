75f9fba09c634b98bde98275445e58a3baf175e9
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 06:08:56 +0100

    Update 10-2.py

--- 10-2.py
+++ 10-2.py
@@ -1,10 +1,8 @@
-not to be a jokeman but this one is kinda stupid....
-basics you just have to copy the code they gave you and then correct the 1 and only mistake
+actual jokeman exercise
+you just have to copy the code they gave you and then correct the 1 and only mistake
 
 easy exercise for the win!
 
-if that's still too much effort:
 change the symbol on line 4 from < to >
 
-(best to keep the comment)
 while n > 0: # while n is greater than 0 (zero), execute the below routine in iterations
