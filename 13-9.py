fe921acba506c48f2fc1f9615e842c63a8d63675
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 03:21:19 +0100

    Create 13-9.py

---
+++ 13-9.py
@@ -0,0 +1,6 @@
+def greeting(name):
+    print('Hello, %s!' % name)
+
+greeting('Bob') # “Hello, Bob!” is printed.
+# Call the function `greeting` once more and print “Hello, Alice!”
+greeting('Alice')
