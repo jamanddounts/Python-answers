91e5991cd88838b6f17a2630eea4330a542f42e2
jamanddounts <71227403+jamanddounts@users.noreply.github.com>
2021-06-22 03:31:28 +0100

    Create 13-12.py

---
+++ 13-12.py
@@ -0,0 +1,7 @@
+# Define the function print_square_number to receive an argument `n`
+def print_square_number(n):
+# Pass a value to argument `n` at the time of call and print on screen “the square of `n`: the value of `n` squared ”
+    print("%s squared:" % (n),n*n)
+
+print_square_number(2)
+print_square_number(3)
